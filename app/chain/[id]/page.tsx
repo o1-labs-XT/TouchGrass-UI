c76d20710d1b065e19d1178cf95ecb5037ff470a
ymekuria <ymekuria@gmail.com>
2025-10-01 01:25:54 -0400

    Add Card component to chain info

--- app/chain/[id]/page.tsx
+++ app/chain/[id]/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation';
 import { getChain, getSubmissionsByChain } from '../../lib/backendClient';
 import type { Chain, Submission } from '../../lib/backendClient';
 import Button from '../../components/Button';
+import Card from '../../components/Card';
 import styles from './ChainDetail.module.css';
 
 export default function ChainDetailPage({ params }: { params: Promise<{ id: string }> }) {
@@ -68,12 +69,12 @@ export default function ChainDetailPage({ params }: { params: Promise<{ id: stri
         </div>
 
       {chain && (
-        <div>
+        <Card>
           <p>{chain.length} images in chain</p>
           <Button variant="primary" onClick={() => router.push('/submit')}>
             Extend This Chain
           </Button>
-        </div>
+        </Card>
       )}
 
       <div>
