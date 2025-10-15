89848b108d5f2beb0e1ebab64b11deb0caa38399
ymekuria <ymekuria@gmail.com>
2025-10-14 18:55:24 -0700

    Add WalletConnect interfaces

--- app/lib/walletConnect.ts
+++ app/lib/walletConnect.ts
@@ -6,3 +6,18 @@ const web3Modal = new Web3Modal({
   projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
   standaloneChains: ["mina:mainnet", "mina:devnet", "zeko:testnet"],
 });
+
+export interface WalletConnectClient extends InstanceType<typeof SignClient> {
+  session: InstanceType<typeof SignClient>["session"];
+}
+
+interface SessionEventParams {
+  event: { name: string; data: any };
+  chainId: string;
+}
+
+interface SessionEvent {
+  id: number;
+  topic: string;
+  params: SessionEventParams;
+}
