80ba6881076d2ab45ca7e01825a82cbc767e3c27
Coby <coby@o1labs.org>
2025-10-28 12:17:27 -0400

    update hardcoded public key

--- app/lib/staticEcdsaKeys.ts
+++ app/lib/staticEcdsaKeys.ts
@@ -1,11 +1,11 @@
 /**
  * Static ECDSA public key coordinates for server-side signing
- * 
+ *
  * This public key corresponds to the private key stored in ECDSA_PRIVATE_KEY
  * environment variable. All image signatures will be generated using this
  * static keypair instead of per-user generated keypairs.
  */
 export const STATIC_ECDSA_PUBLIC_KEY = {
-  x: 'd9e9bbfefb7b78124a1765a25b112293e02da05572f8e91a096b8f6aa9dfa013',
-  y: '319e75646360183c3508a5080e910ca6c61cf7c3eaac8053f15c47d9db66fd24'
+  x: "0e43b3c5e43030ab054cebadca1c6f8817cdccbd6fbefde18eb00287eabd1f12",
+  y: "75181cef858c48cdaa6b54066748b05cacf20027001634cfa3d48d165ed5d93a",
 };
