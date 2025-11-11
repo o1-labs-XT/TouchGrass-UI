ed3654075d9e8907865273583bbd1fb06da96cd5
ymekuria <ymekuria@gmail.com>
2025-11-10 23:42:54 -0800

    Add TooltipProvider component

--- app/components/Tooltip.tsx
+++ app/components/Tooltip.tsx
@@ -2,3 +2,10 @@
 
 import * as React from "react";
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+
+function TooltipProvider({
+  delayDuration = 0,
+  ...props
+}: React.ComponentProps<typeof TooltipPrimitive.Provider>) {
+  return <TooltipPrimitive.Provider delayDuration={delayDuration} {...props} />;
+}
